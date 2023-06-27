37eb7e3a047588a5a6464881c113e8a9dd12d509
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-06-27 15:22:21 +0300

    add link to list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Inter } from "next/font/google";
 // import styles from "@/styles/Home.module.css";
 import LinksList from "@/components/LinksList";
+import CreateLink from "@/components/CreateLink";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
+      <CreateLink />
       <LinksList />
     </main>
   );
